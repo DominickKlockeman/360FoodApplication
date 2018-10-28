0e94649d0ece716793dcc93d6dac4b3e65365b68
terencecaiga <terencecaiga@gmail.com>
2018-10-28 04:25:33 -0700

    added checkout page

--- app/src/main/java/com/example/dominickklockeman/a360foodapp/MainActivity.java
+++ app/src/main/java/com/example/dominickklockeman/a360foodapp/MainActivity.java
@@ -29,8 +29,11 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent checkoutIntent = new Intent(MainActivity.this,
+                        CheckoutActivity.class);
+                startActivity(checkoutIntent);
             }
         });
 
