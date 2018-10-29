7dcc03543afc5815d03cd3190b15848631e05807
terencecaiga <terencecaiga@gmail.com>
2018-10-28 22:19:49 -0700

    added some things to the checkout page

--- app/src/main/java/com/example/dominickklockeman/a360foodapp/CheckoutActivity.java
+++ app/src/main/java/com/example/dominickklockeman/a360foodapp/CheckoutActivity.java
@@ -1,12 +1,13 @@
 package com.example.dominickklockeman.a360foodapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+//import android.view.View;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -14,7 +15,7 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_checkout);
+        Toolbar toolbar = findViewById(R.id.toolbar_checkout);
         setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
--- app/src/main/java/com/example/dominickklockeman/a360foodapp/MainActivity.java
+++ app/src/main/java/com/example/dominickklockeman/a360foodapp/MainActivity.java
@@ -4,7 +4,7 @@ package com.example.dominickklockeman.a360foodapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
